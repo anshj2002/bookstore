cae2d20c2efdce8705e6dcb8ecfa0d1e0452e14f
omaxe <Colocal.dubai@gmail.com>
2025-02-03 11:33:14 +0530

    certain_fixes

--- vir/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from passlib.context import CryptContext
-from routers import Users, books
-pwd_context = CryptContext(schemes = ["bcrypt"] , deprecated = "auto")
-app = FastAPI()
-
-app.include_router(Users.router)
-app.include_router(books.router)
